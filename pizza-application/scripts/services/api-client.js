85122ad45312440554f0998431d9037e5bdaa8ba
sanctionedpranav <pranav9728@gmail.com>
2025-11-17 23:32:50 +0530

    network call function

--- pizza-application/scripts/services/api-client.js
+++ pizza-application/scripts/services/api-client.js
@@ -1,1 +1,15 @@
 // Network API client for Pizza Application
+
+import { URL } from "../../utils/constant";
+
+function networkCall() {
+  const data = fetch(URL);
+
+  console.log("Promise is pending", data);
+  data.then((res) => {
+    console.log("Response is: ", res);
+  }).catch((err) => {
+    console.log("Error is: ", err);
+  });
+  console.log("Bye");
+}
